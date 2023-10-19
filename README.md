b07c099dd49cf9ca8d0951243f3f1581787acbaf
Manoj-M-V <manojvrao28@gmail.com>
2023-10-19 22:52:32 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Quiz-App
+# Quiz App
+
+This is a simple quiz application built using HTML, CSS, and JavaScript. It allows you to answer a series of questions and provides feedback on your performance. The app is a basic template that you can customize and expand according to your needs.
+
+## Getting Started
+
+To run this Quiz App, follow these steps:
+
+1. Follow the website : 
+
+1. Clone the repository to your local machine:
+
+   ```shell
+   git clone https://github.com/Manoj-M-V/Quiz-App.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```shell
+   cd Quiz-App
+   ```
+
+3. Open the `index.html` file in your web browser to start using the app.
+
+## Features
+
+- The app presents a set of questions with multiple-choice answers.
+- Users can select one answer per question and move to the next question.
+- The app provides immediate feedback on whether the selected answer is correct or not.
+- At the end of the quiz, the user's score is displayed.
+
+## Customization
+
+You can customize the app by modifying the `questions` array in the JavaScript code. Add your own questions, answer choices, and correct answers to create your unique quiz. Additionally, you can style the app further by modifying the `style.css` file.
+
+## Code Structure
+
+- `index.html`: The main HTML file that structures the app's user interface.
+- `style.css`: The CSS file for styling the app.
+- `index.js`: The JavaScript code that powers the quiz functionality.
+
+## Contributors
+
+- [Manoj M V](https://github.com/Manoj-M-V/)
+
+## License
+
+This project is open-source and available under the [MIT License](LICENSE). Feel free to use and modify it for your purposes.
+
+
+Enjoy using your Quiz App! If you have any questions or suggestions, please feel free to reach out to me.
---
+++ index.js
@@ -0,0 +1,127 @@
+const questions = [
+    {
+        question: "What is my name?",
+        answers: [
+            { text: "MJ", correct: false },
+            { text: "Manoj", correct: false },
+            { text: "Schelon", correct: false },
+            { text: "GOD", correct: true },
+        ]
+    },
+    {
+        question: "What is my sister's name?",
+        answers: [
+            { text: "Shan", correct: false },
+            { text: "Shanthala", correct: false },
+            { text: "Shanthala putti", correct: false },
+            { text: "Handi", correct: true },
+        ]
+    },
+    {
+        question: "What is my mommy's name?",
+        answers: [
+            { text: "Amma", correct: false },
+            { text: "Mummy", correct: false },
+            { text: "gummi", correct: false },
+            { text: "Mamdy", correct: true },
+        ]
+    },
+    {
+        question: "What is my laptop's name?",
+        answers: [
+            { text: "hehe", correct: false },
+            { text: "adb", correct: false },
+            { text: "jdbfj", correct: false },
+            { text: "jdfn", correct: true },
+        ]
+    },
+];
+
+
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementById("ans-btn");
+const nextButton = document.getElementById("next-btn");
+const answerButtons = document.getElementById("ans-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestions();
+}
+
+function showQuestions() {
+
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex+1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button  = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = "true";
+    });
+    nextButton.style.display = "block";
+}
+
+nextButton.addEventListener("click", function () {
+    if(currentQuestionIndex<questions.length) {
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+});
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestions();
+    } else {
+        showScore();
+    }
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `Your score is ${score} out of ${questions.length} !`;
+    nextButton.innerHTML= "Play Again";
+    nextButton.style.display = "block";
+}
+
+startQuiz();
